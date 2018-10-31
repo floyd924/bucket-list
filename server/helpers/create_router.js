ac1b26758bac7647ade3b77fd980e1283b41765e
Monica Mateiu <monica.mateiu@gmail.com>
2018-10-31 10:16:36 +0000

    creates all routes

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -5,6 +5,7 @@ const createRouter = function(collection) {
 
   const router = express.Router();
 
+//INDEX
   router.get('/', (req, res) => {
     collection
     .find()
@@ -17,7 +18,63 @@ const createRouter = function(collection) {
     });
   });
 
-  
+  //SHOW
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+      .findOne({_id: ObjectID(id) })
+      .then( (docs) => res.json(docs) )
+      .catch( (err) => {
+        console.error(err);
+        res.status(500);
+        res.json({status: 500, error: err});
+      });
+  });
+
+  //CREATE
+  router.post('/', (req, res) => {
+    const newData = req.body;
+    collection.insertOne(newData)
+    .then(() => collection.find().toArray())
+    .then( (docs) => res.json(docs) )
+    .catch( (err) => {
+      console.error(err);
+      res.status(500);
+      res.json({status: 500, error: err});
+    });
+  });
+
+  //DELETE
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+      .deleteOne({_id: ObjectID(id)})
+      .then(() => collection.find().toArray())
+      .then( (docs) => res.json(docs) )
+      .catch( (err) => {
+        console.error(err);
+        res.status(500);
+        res.json({status: 500, error: err});
+      });
+  });
+
+  //UPDATEW
+  router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const updateData = req.body;
+    collection
+      .updateOne(
+        {_id: ObjectID(id)},
+        {$set: updateData}
+      )
+      .then(() => collection.find().toArray())
+      .then( (docs) => res.json(docs) )
+      .catch( (err) => {
+        console.error(err);
+        res.status(500);
+        res.json({status: 500, error: err});
+      });
+  });
 
   return router;
 
